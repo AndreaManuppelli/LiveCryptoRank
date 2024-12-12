7ec3b9f82cb558d8a989a414eae410445e659199
AndreaManuppelli <andreamanuppelli@gmail.com>
2024-12-12 06:30:36 +0100

    Add CryptoService to HomeComponent and fetch top coins on init.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CryptoService } from '../../services/crypto.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  coins: any[] = [];
 
+
+  constructor(private cryptoService: CryptoService) { }
+
+  
+
+  ngOnInit() {
+    this.cryptoService.getTopCoins().subscribe(
+      coins => {
+        this.coins = coins;
+        console.log(this.coins);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
 }
