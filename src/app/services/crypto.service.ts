d3a4980ceb3682c03408284b644e65fc339f7974
AndreaManuppelli <andreamanuppelli@gmail.com>
2024-12-12 06:27:13 +0100

    Add HttpClient to fetch and map top coins data from CoinGecko API.

--- src/app/services/crypto.service.ts
+++ src/app/services/crypto.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CryptoService {
 
-  constructor() { }
+  private apiUrl = 'https://api.coingecko.com/api/v3/coins/markets';
+
+  constructor(private http: HttpClient) { }
+
+  getTopCoins(limit: number = 50): Observable<any[]> {
+    const params = {
+      vs_currency: 'usd',
+      order: 'market_cap_desc',
+      per_page: limit.toString(),
+      page: '1',
+      sparkline: 'false'
+    };
+
+    return this.http.get<any[]>(this.apiUrl, { params }).pipe(
+      map(coins => coins.map(coin => ({
+        id: coin.id,
+        name: coin.name,
+        symbol: coin.symbol,
+        image: coin.image,
+        current_price: coin.current_price,
+        market_cap: coin.market_cap
+      })))
+    );
+  }
 }
