e043b4b8c4d5418127882165974ffab69b1304e2
AndreaManuppelli <andreamanuppelli@gmail.com>
2024-12-12 06:41:03 +0100

    Add CoinCardComponent to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CoinCardComponent } from './components/coin-card/coin-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CoinCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/coin-card/coin-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coin-card',
+  templateUrl: './coin-card.component.html',
+  styleUrl: './coin-card.component.scss'
+})
+export class CoinCardComponent {
+
+}
