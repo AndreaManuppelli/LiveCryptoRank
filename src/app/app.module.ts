a2842afa919a87db2d4bcf6de6f7c09ec76bf343
AndreaManuppelli <andreamanuppelli@gmail.com>
2024-12-12 06:35:09 +0100

    Add LucideAngularModule with icon support to AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LucideAngularModule, icons} from 'lucide-angular';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    LucideAngularModule.pick(icons)
   ],
   providers: [],
   bootstrap: [AppComponent]
