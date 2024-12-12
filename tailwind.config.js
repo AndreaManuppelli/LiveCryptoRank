49d2e8e8fb1f5e1c61545101e0225a000e2516d8
AndreaManuppelli <andreamanuppelli@gmail.com>
2024-12-12 06:26:07 +0100

    Add custom color palette and update content paths in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
+import getPalette from "tailwindcss-palette-generator";
+
+
+
+const palette = getPalette([
+  {
+    color: "#ff007a",
+    name: "primary",
+    shades: [100, 200, 300, 400, 450, 500, 600, 700, 800, 900]
+  },
+  {
+    color: "#000000",
+    name: "secondary",
+    shades: [100, 200, 300, 400, 450, 500, 600, 700, 800, 900]
+  }
+]);
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...palette
+      }
+    },
   },
   plugins: [],
 }
